2f9450a14939873c5122ddd9d94b0910b68ae1d1
Mansiks33 <88619679+Mansiks33@users.noreply.github.com>
2021-12-26 20:19:15 +0530

    Create global.R

---
+++ global.R
@@ -0,0 +1,9 @@
+# Variables that can be put on the x and y axes
+axis_vars <- c(
+  "Rating Meter" = "Meter",
+  "Numeric Rating" = "Rating",
+  "Number of reviews" = "Reviews",
+  "Crore at box office" = "BoxOffice",
+  "Year" = "Year",
+  "Length (minutes)" = "Runtime"
+)
